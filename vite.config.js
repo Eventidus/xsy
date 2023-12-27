95ebafec11c7857fd87a8f8dd04eb0e2744cc9c2
Liuzx <1024jianghu@gmail.com>
2023-12-27 22:58:23 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
+  base: '/xsy/',
   build: {
     outDir: 'docs' // 打包文件的输出目录
   }
